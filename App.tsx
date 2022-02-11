a801b6d0f95ec931c58446cdcd22258a58315b7d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-02-11 19:45:19 -0300

    chore: configured styled-components

--- App.tsx
+++ App.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title="GoFinances" />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
