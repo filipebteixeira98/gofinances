c0157c87c277af4ebd2d1463c11999dd35a1f954
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-02-12 22:07:10 -0300

    chore: added ThemeProvider

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
+`;
